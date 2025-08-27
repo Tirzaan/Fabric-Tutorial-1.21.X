ae05379dacba3c3d68c3b12c85234c386bb14b77
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-27 14:41:31 -0600

    First Commit

--- src/main/java/net/tirzaanneil/tutorialmoding/TutorialMod.java
+++ src/main/java/net/tirzaanneil/tutorialmoding/TutorialMod.java
@@ -2,23 +2,21 @@ package net.tirzaanneil.tutorialmoding;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tirzaanneil.tutorialmoding.block.ModBlocks;
+import net.tirzaanneil.tutorialmoding.item.ModItemGroups;
+import net.tirzaanneil.tutorialmoding.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "tutorial-mod"; //Change to tutorialmod?
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        ModItemGroups.registerItemGroups();
 
-		LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/tirzaanneil/tutorialmoding/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.tirzaanneil.tutorialmoding;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
